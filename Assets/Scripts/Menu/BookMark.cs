a3146478f4ba26c5bb9047eea0ee54cff0198419
BlueTonio <82876244+BlueTonio@users.noreply.github.com>
2023-01-29 12:08:55 +0100

    menu change

---
+++ Assets/Scripts/Menu/BookMark.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BookMark : MonoBehaviour
+{
+    [SerializeField]float speed;
+    float counter;
+    [SerializeField] LayerMask bookMark;
+    [SerializeField] Transform target;
+    [SerializeField] Transform starPos;
+    Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+        counter = 0;
+    }
+
+   
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, 1000, bookMark))
+        {
+            counter += speed * Time.deltaTime;
+            counter = Mathf.Clamp01(counter);
+            transform.position = Vector3.right * Mathf.Lerp(transform.position.x, target.position.x, counter) + Vector3.forward * transform.position.z;
+        }
+        else
+        {
+            counter -= speed * Time.deltaTime;
+            counter = Mathf.Clamp01(counter);
+            transform.position = Vector3.right * Mathf.Lerp(starPos.position.x, transform.position.x, counter) + Vector3.forward * transform.position.z;
+        }
+    }
+
+
+}
