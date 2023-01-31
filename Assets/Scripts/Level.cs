9b7f35a53d84d3ab14ce055e6c6306ac7a380bc6
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2023-01-31 02:32:28 +0100

    RenderTexture/Book position converting

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] public Transform[] mobs;
+    [SerializeField] public Camera camera;
+    [SerializeField] public SpriteRenderer sprite;
+    [SerializeField] public BoxCollider collider;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/LevelMgr.cs
+++ Assets/Scripts/LevelMgr.cs
@@ -8,17 +8,18 @@ public class LevelMgr : Singleton<LevelMgr>
     [System.Serializable]
     public class KeyValuePair {
         public int page;
-        public MapObject mapObject;
+        public Level mapObject;
     }
-    [System.Serializable]
-    public class MapObject {
-        public GameObject levelObject;
-        public Texture2D movingTexture;
-        public Texture2D finalTexture;
-    }
+    
     [SerializeField] List<KeyValuePair> MyList = new List<KeyValuePair>();
     EndlessBook book;
-    Dictionary<int, MapObject> dic = new Dictionary<int, MapObject>();
+    Dictionary<int, Level> dic = new Dictionary<int, Level>();
+
+    float baseTotalPageLength = 39.2f * 0.01f; //cm
+    float totalPageLength;
+    float pageLength;
+    float notViewablePageLength;
+    [SerializeField] bool clickMe;
     void Awake() {
         foreach (var kvp in MyList) {
             dic[kvp.page] = kvp.mapObject;
@@ -28,11 +29,16 @@ public class LevelMgr : Singleton<LevelMgr>
     void Start()
     {
         book = EndlessBookMgr.Instance.book;
+        totalPageLength = baseTotalPageLength * book.transform.localScale.x;
+        pageLength = totalPageLength * 0.8037109375f;
+
+        //pageLength = basePageLength * book.transform.localScale.x;
+        notViewablePageLength = totalPageLength * 0.1962890625f;
+        //totalPageLength = pageLength + notViewablePageLength;
+        OpenToPage(3);
     }
 
     public void OpenToPage(int panegN) {
-
-        book.pages[panegN].PageFrontMaterial.mainTexture = dic[panegN].movingTexture;
         book.TurnToPage(panegN, EndlessBook.PageTurnTimeTypeEnum.TimePerPage, 0.5f,
                     openTime: 1f,
                     onCompleted: OnBookTurnToPageCompleted,
@@ -51,11 +57,40 @@ public class LevelMgr : Singleton<LevelMgr>
     }
 
     protected virtual void OnPageTurnEnd(Page page, int pageNumberFront, int pageNumberBack, int pageNumberFirstVisible, int pageNumberLastVisible, Page.TurnDirectionEnum turnDirection) {
+        
+        Vector3 sizeOfCameraPage = dic[pageNumberFront].collider.size;
+        foreach (Transform item in dic[pageNumberFront].mobs) {
+            item.rotation = Quaternion.Euler(Vector3.right * 90);
+            Vector3 cameraPagePosition = item.localPosition;
+            Vector3 clampedPos;
+            clampedPos.x = cameraPagePosition.x / (sizeOfCameraPage.x * 0.5f);
+            clampedPos.y = cameraPagePosition.y / (sizeOfCameraPage.y * 0.5f);
+            clampedPos.z = 0;
+
+
+
+            Vector3 bookPagePosition;
+            bookPagePosition.x = clampedPos.x * totalPageLength * 0.5f + 0.65f; //boh
+            bookPagePosition.z = clampedPos.y * totalPageLength * 0.5f;
+            bookPagePosition.y = 3.6f * 0.01f * book.transform.localScale.y;
+            item.position = bookPagePosition + Vector3.left * (totalPageLength *0.5f - notViewablePageLength);
+
+            item.localScale *= totalPageLength / sizeOfCameraPage.x;
+
+        }
+
+
+        //TODO also left
+
+
         Debug.Log("OnPageTurnEnd: front [" + pageNumberFront + "] back [" + pageNumberBack + "] fv [" + pageNumberFirstVisible + "] lv [" + pageNumberLastVisible + "] dir [" + turnDirection + "]");
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (clickMe) {
+            clickMe = false;
+            OpenToPage(1);
+        }
     }
 }
