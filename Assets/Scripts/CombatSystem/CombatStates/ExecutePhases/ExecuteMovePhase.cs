873a96141c0f04b6a163522d23502118aa98e4e1
Stefano Canavesi <ultratails2001@gmail.com>
2023-02-03 22:52:06 +0100

    Fixed broken fight + cam

--- Assets/Scripts/CombatSystem/CombatStates/ExecutePhases/ExecuteMovePhase.cs
+++ Assets/Scripts/CombatSystem/CombatStates/ExecutePhases/ExecuteMovePhase.cs
@@ -7,12 +7,18 @@ public class ExecuteMovePhase : StateMachineBehaviour
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         //Check if enemies needs to be skipped
         for(int i = FightManager.Instance.currentTurn; i < FightManager.Instance.enemiesToSpawn.Length; i++) {
+            Debug.Log(i);
             if (!FightManager.Instance.enemies[i].IsDead) {
                 break;
             }
             FightManager.Instance.currentTurn++;
         }
 
+        if(FightManager.Instance.currentTurn >= FightManager.Instance.enemiesToSpawn.Length) {
+            animator.SetBool("AllEnemiesActed", true);
+            FightManager.Instance.NextPhase();
+            return;
+        }
         //Check if all enemies are dead
         if (FightManager.Instance.CheckIfAllEnemiesAreDead()) {
             animator.SetBool("FightHasEnded", true);
