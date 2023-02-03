5b1473198b9263279cd4eb6a8da1e95ccb6b05b7
Stefano Canavesi <ultratails2001@gmail.com>
2023-02-03 01:42:21 +0100

    Scriptable update

--- Assets/Scripts/ScriptableObjects/Roots/RootData.cs
+++ Assets/Scripts/ScriptableObjects/Roots/RootData.cs
@@ -2,15 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Avaiability {
-    World,
-    Combat
-}
 
 [CreateAssetMenu(fileName = "RootWorldData", menuName = "Roots/World")]
 public class RootData : ScriptableObject
 {
     public string root;
-    public Avaiability context = Avaiability.World;
+    public GameState context = GameState.World;
     public string[] suffixes;
 }
