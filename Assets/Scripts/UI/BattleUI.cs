d34454e1b64582a2fa56d2bdfb4aa61e979755fb
BlueTonio <82876244+BlueTonio@users.noreply.github.com>
2023-02-03 19:24:27 +0100

    Finish the UIpain

--- Assets/Scripts/UI/BattleUI.cs
+++ Assets/Scripts/UI/BattleUI.cs
@@ -23,16 +23,35 @@ public class BattleUI : MonoBehaviour
     
     void Update()
     {
+        
         if(FightManager.Instance.currentTurn == -1)
         {
+            
             myrect.localPosition = new Vector3(myrect.localPosition.x,Mathf.Lerp(myrect.localPosition.y, target.localPosition.y, speed * Time.deltaTime), myrect.localPosition.z);
             for (int i = 0; i < FightManager.Instance.enemies.Length; i++)
             {
                 if (!FightManager.Instance.enemies[i])
                 {
                     buttons[i].gameObject.SetActive(false);
-                    break;
+                    continue;
                 }
+                else
+                {
+                    buttons[i].gameObject.SetActive(true);
+                }
+                if (!FightManager.Instance.PlayerFightingController.currentSelectedRoot)
+                {
+                    buttons[i].interactable = false;
+                }
+                else
+                {
+                    buttons[i].interactable = true;
+                }
+                if (FightManager.Instance.enemies[i].IsDead)
+                {
+                    buttons[i].interactable = false;
+                }
+                
                 buttons[i].GetComponent<Image>().sprite = FightManager.Instance.enemies[i].Icon;
             }
             if (FightManager.Instance.PlayerFightingController.currentSelectedRoot)
@@ -51,5 +70,13 @@ public class BattleUI : MonoBehaviour
         {
             myrect.localPosition = new Vector3(myrect.localPosition.x, Mathf.Lerp(myrect.localPosition.y, oldRect.y, speed * Time.deltaTime),  myrect.localPosition.z);
         }
+    }
+
+
+    public void SelectEnemy(int index)
+    {
+        FightManager.Instance.PlayerFightingController.target = FightManager.Instance.enemies[index];
+        FightManager.Instance.NextPhase();
+        
     }
 }
