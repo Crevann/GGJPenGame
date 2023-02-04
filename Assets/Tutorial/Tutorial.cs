29b310bee92346957f1513386c4c5b0036af3500
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2023-02-04 12:11:01 +0100

    StartingTutorial

---
+++ Assets/Tutorial/Tutorial.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.AI;
+
+public class Tutorial : Singleton<Tutorial>
+{
+    private enum State { LearnToMove, LearnToPickUp, GoToFight, JumpInTheWhole}
+    [SerializeField] TeleportingObject CaneFiore;
+    [SerializeField] AnimationCurve standUpMovement;
+    [SerializeField] float timeToRaise = 1;
+    [SerializeField] float fadeTime = 1;
+    [SerializeField] Transform[] fiorePositions;
+
+    [Header("Events:")]
+    bool done = false;
+    [SerializeField] UnityEvent startTutorial;
+    [SerializeField] UnityEvent moved;
+    [SerializeField] UnityEvent pickedUp;
+    [SerializeField] UnityEvent fought;
+    [SerializeField] UnityEvent jumped;
+
+
+    private State currentState = State.LearnToMove;
+
+    PlayerMovement player;
+
+
+
+    float counter;
+    bool isRising;
+    float risingVal = 0;
+    NavMeshAgent fioreAgent;
+    NavMeshAgent penAgent;
+
+    private void Start() {
+        fioreAgent = CaneFiore.GetComponent<NavMeshAgent>();
+        fioreAgent.enabled = false;
+    }
+    void Update()
+    {
+        if (!player || !player.isActiveAndEnabled) {
+            player = FindObjectOfType<PlayerMovement>();
+            if(player) penAgent = player.GetComponent<NavMeshAgent>();
+            done = false;
+            return;
+        }
+        Rise();
+        Do();
+    }
+    void Do() {
+        if (risingVal < 1) return;
+        fioreAgent.destination = fiorePositions[(int)currentState].position;
+        switch (currentState) {
+            case State.LearnToMove:
+                if (!done) {
+                    startTutorial.Invoke();
+                    done = true;
+                }
+                if (penAgent.velocity.sqrMagnitude > 0) {
+                    done = false;
+                    currentState = State.LearnToPickUp;
+                    moved.Invoke();
+                }
+                break;
+            case State.LearnToPickUp:
+                if (Inventory.Instance.roots[0] != null) {
+                    currentState = State.GoToFight;
+                    pickedUp.Invoke();
+                }
+                break;
+            case State.GoToFight:
+                if(GameMGR.Instance.CurrentState == GameState.Fight) {
+                    currentState = State.JumpInTheWhole;
+                    fought.Invoke();
+                }
+                break;
+            case State.JumpInTheWhole:
+                break;
+            default:
+                break;
+        }
+
+    }
+    void Rise() {
+        if (risingVal < 1 && CaneFiore.onTopOfBook) {
+            if (!isRising) {
+                fioreAgent.enabled = true;
+                counter = 0;
+                isRising = true;
+            }
+            counter += Time.deltaTime;
+            risingVal = counter / timeToRaise;
+            risingVal = Mathf.Clamp01(risingVal);
+            CaneFiore.transform.GetChild(0).rotation = Quaternion.Euler(Vector3.right * standUpMovement.Evaluate(risingVal) * 90);
+            if (!fioreAgent.enabled && risingVal >= 1) {
+                //CaneFiore.transform.GetChild(0).rotat = Quaternion.Euler(Vector3.right * 90);
+            }
+        }
+
+    }
+}
