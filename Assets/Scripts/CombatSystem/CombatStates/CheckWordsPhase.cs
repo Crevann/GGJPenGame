18086773cef1fb12b9fa669448d11c7b55faec28
Stefano Canavesi <ultratails2001@gmail.com>
2023-02-04 01:21:34 +0100

    Removed check phase

--- Assets/Scripts/CombatSystem/CombatStates/CheckWordsPhase.cs
+++ Assets/Scripts/CombatSystem/CombatStates/CheckWordsPhase.cs
@@ -6,13 +6,13 @@ public class CheckWordsPhase : StateMachineBehaviour
 {
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         //This will enable the UI
-        if (FightManager.Instance.CheckTargetWords()) {
+        /*if (FightManager.Instance.CheckTargetWords()) {
             Debug.Log("Words are the same, engage stall");
             //animator.SetTrigger("EngageStall");
             FightManager.Instance.NextPhase();
         }
         else {
             FightManager.Instance.NextPhase();
-        }
+        }*/
     }
 }
