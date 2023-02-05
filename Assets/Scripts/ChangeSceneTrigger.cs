87e327975a98d2d33c29e407841c93c91aa2108c
Stefano Canavesi <ultratails2001@gmail.com>
2023-02-05 01:21:35 +0100

    Keeps current level

--- Assets/Scripts/ChangeSceneTrigger.cs
+++ Assets/Scripts/ChangeSceneTrigger.cs
@@ -22,6 +22,7 @@ public class ChangeSceneTrigger : MonoBehaviour {
         if (isCounting) {
             count += Time.deltaTime;
             if (count > delayTime) {
+                GameMGR.Instance.currentLevel = pageToTurnTo;
                 LevelMgr.Instance.OpenToPage(pageToTurnTo);
                 isCounting = false;
                 p.JumpOut(whereToGo);
